348b32a4fbb00d6724ea39a42d1c732df3cc9546
adarsh12sharma-star <adarsh12sharma234@gmail.com>
2026-02-05 15:35:45 +0530

    Refactor GCD function for improved readability

--- GCD.c
+++ GCD.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-
 int gcd(int a, int b) {
     int gcd = 1;
-
     for (int i = 1; i <= a && i <= b; i++) {
         if (a % i == 0 && b % i == 0) {
-            gcd = i;
-        }}
-    return gcd;
-}
-
+            gcd = i;}}
+    return gcd;}
 int main() {
     int a, b;
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-
     printf("GCD = %d\n", gcd(a, b));
     return 0;}
